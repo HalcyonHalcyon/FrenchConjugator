43acf9c039cabfa4138449f0fc4881c001c09081
HalcyonHalcyon <emily.potter513@gmail.com>
2025-09-01 16:33:49 +1200

    Trying to make languages talk to each other v4

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,24 @@
 <template>
   <div>
     <h1>French Verb Conjugation Practice</h1>
-    <h2>Verb to conjugate: </h2>
+    <h2>Tense to conjugate: </h2>
   </div>
+  <!--
   <div>
     <b-dropdown id="tense" text="Dropdown Button" class="m-md-2">
-    <b-dropdown-item>Présent</b-dropdown-item>
-    <b-dropdown-item>Futur Simple</b-dropdown-item>
-    <b-dropdown-item>Imparfait</b-dropdown-item>
-    <b-dropdown-item>Passé Composé</b-dropdown-item>
-    <b-dropdown-item>Futur Antérieur</b-dropdown-item>
-    <b-dropdown-item>Plus-que-parfait</b-dropdown-item>
-    <b-dropdown-item>Subjonctif</b-dropdown-item>
-    <b-dropdown-item>Conditionnel</b-dropdown-item>
-    <b-dropdown-item>Conditionnel passé</b-dropdown-item>
+      <b-dropdown-item>Présent</b-dropdown-item>
+      <b-dropdown-item>Futur Simple</b-dropdown-item>
+      <b-dropdown-item>Imparfait</b-dropdown-item>
+      <b-dropdown-item>Passé Composé</b-dropdown-item>
+      <b-dropdown-item>Futur Antérieur</b-dropdown-item>
+      <b-dropdown-item>Plus-que-parfait</b-dropdown-item>
+      <b-dropdown-item>Subjonctif</b-dropdown-item>
+      <b-dropdown-item>Conditionnel</b-dropdown-item>
+      <b-dropdown-item>Conditionnel passé</b-dropdown-item>
     <b-dropdown-divider></b-dropdown-divider>
   </b-dropdown>
   </div>
+-->
 </template>
 
 <style scoped>
