df70f4bebb4496630ed03b9a99f366a668fa6337
HalcyonHalcyon <emily.potter513@gmail.com>
2025-09-08 13:24:39 +1200

    Altering router functionality

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import App from '../App.vue'
 import Home from '../Home.vue'
 import Quiz from '../Quiz.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/quiz', component: Quiz },
+  { path: '/', component: App},
+  { path: '/home', component: Home },
+  { path: '/quiz', component: Quiz }
 ]
 
 const router = createRouter({
