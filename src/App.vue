48c0ecb014245b1aadc64e9fe531fdd48682d313
HalcyonHalcyon <emily.potter513@gmail.com>
2025-09-08 13:34:57 +1200

    Changing router functionality

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,9 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import { Verb } from './models/Verb'
 
-</script>
 
 <template>
   <div>
-    <!--<h1>French Conjugation Practice</h1>
-
-    <router-view></router-view>-->
-    <div id="app"></div>
-  <!--<script type="module" src="/src/main.ts"></script>-->
-  <div>
-    <h1>French Verb Conjugation Practice</h1>
-    <h4>Use the settings below to start a French conjugation practice quiz</h4>
-    <label for="tense"><h3>Choose a tense to conjugate: </h3></label>
-    <select name="tense" id="tense">
-      <optgroup label="Indicative - Simple">
-        <option value="Présent">Présent</option>
-        <option value="Futur Simple">Futur</option>
-        <option value="Imparfait">Imparfait</option>
-      </optgroup>
-      <optgroup label="Indicative - Perfect">
-        <option value="Passé Composé">Passé Composé</option>
-        <option value="Futur Antérieur">Futur Antérieur</option>
-        <option value="Plus-que-parfait">Plus-que-parfait</option>
-      </optgroup>
-      <optgroup label="Subjunctive">
-        <option value="Subjonctif">Subjonctif présent</option>
-      </optgroup>
-      <optgroup label="Conditional">
-        <option value="Conditionnel">Conditionnel présent</option>
-        <option value="Conditionnel passé">Conditionnel passé</option>
-      </optgroup>
-    </select>
-  </div>
-  <div></div>
-  <br>
-  <!--<div>
-    <button type="button" onclick="window.location.href='quiz.vue'">Start Quiz</button>
-  </div>-->
-    <router-link to="/quiz">
-      <button>Start Quiz</button>
-    </router-link>
+    <h1>French Conjugation Practice</h1>
+    <router-view></router-view>
   </div>
   <!--<div id="app"></div>
   <div>
@@ -75,6 +36,13 @@ import { Verb } from './models/Verb'
     <button type="button" onclick="window.location.href='quiz.vue'">Start Quiz</button>
   </div>-->
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Verb } from './models/Verb'
+
+</script>
+
 <style scoped>
 .logo {
   height: 6em;
