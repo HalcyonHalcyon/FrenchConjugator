915ea0adebd6c7d4012df5f8394ab374828f94fc
HalcyonHalcyon <emily.potter513@gmail.com>
2025-09-10 09:44:48 +1200

    Altering quiz.vue layout

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -3,54 +3,77 @@ import { ref } from 'vue'
 import { Verb } from './models/Verb'
 import { useRoute } from 'vue-router'
 import * as VerbList from './data/VerbList.ts';
+import { isQuestionDotToken } from 'typescript';
 
 VerbList.loadVerbs();
 
 const route = useRoute()
 const selectedTense = route.query.tense
+console.log("Selected tense is:",selectedTense)
 
-const subject: string[] = ["Je", "Tu", "Il", "Nous", "Vous", "Ils"]
+const subject: string[] = ["je", "tu", "il", "nous", "vous", "ils"]
 var index:number;
 function randomSubject(): String {
   index = Math.floor(Math.random() * (5 + 1));
   return subject[index];
 }
 
+function conjugateSetup(): String {
+  const infinitive = ref<string>('');
+  const randomVerb = Verb.randomVerb();
+  if (randomVerb) {
+    infinitive.value = randomVerb.infinitive
+  } else {
+    infinitive.value = '';
+  }
+  const subject = randomSubject();
+  return infinitive.value,subject;
+}
+
+const numQuestions = 10;
+
+
+
 const infinitive1 = ref<string>('');
 const infinitive2 = ref<string>('');
-const infinitive3 = ref<string | undefined>(undefined);
-const infinitive4 = ref<string | undefined>(undefined);
-const infinitive5 = ref<string | undefined>(undefined);
-const infinitive6 = ref<string | undefined>(undefined);
-const infinitive7 = ref<string | undefined>(undefined);
-const infinitive8 = ref<string | undefined>(undefined);
-const infinitive9 = ref<string | undefined>(undefined);
-const infinitive10 = ref<string | undefined>(undefined);
+const infinitive3 = ref<string>('');
+const infinitive4 = ref<string>('');
+const infinitive5 = ref<string>('');
+const infinitive6 = ref<string>('');
+const infinitive7 = ref<string>('');
+const infinitive8 = ref<string>('');
+const infinitive9 = ref<string>('');
+const infinitive10 = ref<string>('');
 
-const randomVerb1 = Verb.randomVerb()
-const randomVerb2 = Verb.randomVerb()
-const randomVerb3 = Verb.randomVerb()
-const randomVerb4 = Verb.randomVerb()
-const randomVerb5 = Verb.randomVerb()
-const randomVerb6 = Verb.randomVerb()
-const randomVerb7 = Verb.randomVerb()
-const randomVerb8 = Verb.randomVerb()
-const randomVerb9 = Verb.randomVerb()
-const randomVerb10 = Verb.randomVerb()
+const randomVerb1 = Verb.randomVerb();
+const randomVerb2 = Verb.randomVerb();
+const randomVerb3 = Verb.randomVerb();
+const randomVerb4 = Verb.randomVerb();
+const randomVerb5 = Verb.randomVerb();
+const randomVerb6 = Verb.randomVerb();
+const randomVerb7 = Verb.randomVerb();
+const randomVerb8 = Verb.randomVerb();
+const randomVerb9 = Verb.randomVerb();
+const randomVerb10 = Verb.randomVerb();
 
-if (randomVerb1) {
-  infinitive1.value = randomVerb1.infinitive
-  infinitive2.value = randomVerb1.infinitive
-  const infinitive3 = randomVerb1.infinitive
-  const infinitive4 = randomVerb1.infinitive
-  const infinitive5 = randomVerb1.infinitive
-  const infinitive6 = randomVerb1.infinitive
-  const infinitive7 = randomVerb1.infinitive
-  const infinitive8 = randomVerb1.infinitive
-  const infinitive9 = randomVerb1.infinitive
-  const infinitive10 = randomVerb1.infinitive
+if (randomVerb1 && randomVerb2 && randomVerb3 && randomVerb4 && randomVerb5) {
+  infinitive1.value = randomVerb1.infinitive;
+  infinitive2.value = randomVerb2.infinitive;
+  infinitive3.value = randomVerb3.infinitive;
+  infinitive4.value = randomVerb4.infinitive;
+  infinitive5.value = randomVerb5.infinitive;
 } else {
-  console.warn("Verb list is empty!")
+  console.warn("Verb list is empty!");
+}
+
+if (randomVerb6 && randomVerb7 && randomVerb8 && randomVerb9 && randomVerb10) {
+  infinitive6.value = randomVerb6.infinitive;
+  infinitive7.value = randomVerb7.infinitive;
+  infinitive8.value = randomVerb8.infinitive;
+  infinitive9.value = randomVerb9.infinitive;
+  infinitive10.value = randomVerb10.infinitive;
+} else {
+  console.warn("Verb list is empty!");
 }
 
 var subject1:String = randomSubject();
@@ -63,6 +86,11 @@ var subject7:String = randomSubject();
 var subject8:String = randomSubject();
 var subject9:String = randomSubject();
 var subject10:String = randomSubject();
+var randomisedVerbs: any[] = Array(numQuestions).fill("");
+
+for (index=0;index<numQuestions;index++) {
+  randomisedVerbs[index] = Verb.randomVerb();
+}
 
 </script>
 
@@ -75,11 +103,28 @@ var subject10:String = randomSubject();
   <div class="question-box">
     <form action="/action_page.php">
         <div class="question">
+          <!--<div v-for="(product, index) in products" :key="product.id">
+            #{{ index + 1 }} - {{ product.name }}
+          </div>-->
+          <!--<div
+            v-for="(q, i) in numQuestions"
+            :key="i"
+            class="question"
+          >
+            const {infinitiveValue, subject} = conjugateSetup();
+            <h2>Question {{ i + 1 }}</h2>
+            <label>
+              Tense: {{ selectedTense }} <br>
+              Verb: {{ infinitiveValue }} <br>
+              Subject: {{ subject }}
+            </label>
+            <br />
+          </div>-->
           <h2>Question 1</h2>
           <label for="q1">
-            Tense: {{ selectedTense }} 
+            Tense: {{ selectedTense }}
             <br>
-            Verb: {{infinitive1}} 
+            Verb: {{ infinitive1 }}
             <br>
             Subject: {{ subject1 }}
           </label>
@@ -90,7 +135,14 @@ var subject10:String = randomSubject();
         </div>
         <div class="question">
           <h2>Question 2</h2>
-          <label for="q2">Tense: {{ selectedTense }} <br>Verb: {{infinitive2}} <br>Subject: {{ subject2 }}</label><br>
+          <label for="q2">
+            Tense: {{ selectedTense }}
+            <br>
+            Verb: {{ infinitive2 }}
+            <br>
+            Subject: {{ subject2 }}
+          </label>
+          <br>
           <input type="text" id="q2" name="q2" value=""><br>
           <input type="button" value="Check">
 
@@ -100,6 +152,7 @@ var subject10:String = randomSubject();
           <router-link to="/">
               <button>Return to home page</button>
           </router-link>
+        
         </div>
     </form>
   </div>
